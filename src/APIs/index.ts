a435932f0756dcb996d52b6868cdcdb51e169288
sonephet <sonephet.dev@gmail.com>
2023-03-14 16:28:21 +0700

    deploy

--- src/APIs/index.ts
+++ src/APIs/index.ts
@@ -1,27 +1,27 @@
 import { Router } from "express";
 import test from "./test";
 // import userApi from "./users";
-// import bookApi from "./book";
-// import bookTypeApi from "./bookTypes";
+import bookApi from "./book";
+import bookTypeApi from "./bookTypes";
 // import provinceApi from "./province";
 // import cityApi from "./city";
 // import calendarApi from "./calendar";
 // import festivalApi from "./festival";
 // import dashboardApi from "./dashboard";
 // import contactApi from "./contact";
-// import khamkhom from "./khamkhom";
+import khamkhom from "./khamkhom";
 
 const router = Router();
 router.use("/", test);
 // router.use("/", userApi);
-// router.use("/", bookApi);
-// router.use("/", bookTypeApi);
+router.use("/", bookApi);
+router.use("/", bookTypeApi);
 // router.use("/", cityApi);
 // router.use("/", provinceApi);
 // router.use("/", festivalApi);
 // router.use("/", calendarApi);
 // router.use("/", dashboardApi);
 // router.use("/", contactApi);
-// router.use("/", khamkhom);
+router.use("/", khamkhom);
 
 export default router;
