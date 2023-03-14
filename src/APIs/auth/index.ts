54cd8e6f60da55134258c9eb3accb8d1e698e5ac
sonephet <sonephet.dev@gmail.com>
2023-03-14 19:38:35 +0700

    deploy

--- src/APIs/auth/index.ts
+++ src/APIs/auth/index.ts
@@ -7,9 +7,9 @@ const router = Router()
 
 router.post('/auth/login', validateLogin, validateResults, userLoginController)
 router.post('/auth/verify', validateVerify, validateResults, userVerifyController)
-router.post("/auth/register", validateUserRegister, validateResults, userRegisterController);
-router.post("/auth/email-send-pin", validateUserSendMail, validateResults, userResendCodeEmailController);
-router.post("/auth/whatsapp-send-pin", validateUserRegister, validateResults, userResendCodeWhatsappController);
+// router.post("/auth/register", validateUserRegister, validateResults, userRegisterController);
+// router.post("/auth/email-send-pin", validateUserSendMail, validateResults, userResendCodeEmailController);
+// router.post("/auth/whatsapp-send-pin", validateUserRegister, validateResults, userResendCodeWhatsappController);
 router.post('/me', verify, getMeController)
 
 export default router
