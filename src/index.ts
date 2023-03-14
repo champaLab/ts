2aa29b8665b1bd2f8694c4041d60d2cafaab70b0
sonephet <sonephet.dev@gmail.com>
2023-03-14 15:53:02 +0700

    deploy

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,12 @@ app.get('/', async (req: any, res: any) => {
     return res.send(pro)
 })
 
+app.get('/a', async (req: any, res: any) => {
+
+    let pro = await prisma.tbl_provinces.findMany()
+    return res.send(pro)
+})
+
 const port = 3000
 app.listen(port, () => {
     console.log("server listening on port " + port)
